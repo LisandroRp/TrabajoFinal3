c14596d1bb1482485c1295ef1296d5149ec7af55
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-03-22 00:54:22 -0300

    heroku17

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import ServiceException from "./src/exceptions/ServiceException.js"
+/* import ServiceException from "./src/exceptions/ServiceException.js"
 import config from './options/config.js'
 import parseArgs from "minimist";
-import { fork } from 'child_process'
+import { fork } from 'child_process' */
 
-import { generalRouter } from "./src/routers/api/GeneralRouter.js";
+/* import { generalRouter } from "./src/routers/api/GeneralRouter.js";
 import { productRouter } from "./src/routers/api/ProductRouter.js";
 import { messageRouter } from "./src/routers/api/MessageRouter.js";
 import { cartRouter } from "./src/routers/api/CartRouter.js";
-import { randomRouter } from "./src/routers/api/RandomRouter.js";
+import { randomRouter } from "./src/routers/api/RandomRouter.js"; */
 
 import { productWebRouter } from "./src/routers/web/ProductWebRouter.js";
 import { messageWebRouter } from "./src/routers/web/MessageWebRouter.js";
@@ -17,47 +17,47 @@ import { infoWebRouter } from "./src/routers/web/InfoWebRouter.js";
 
 import express from 'express'
 import handlebars from 'express-handlebars'
-import session from 'express-session'
+/* import session from 'express-session'
 import sessionFile from 'session-file-store'
-sessionFile(session)
+sessionFile(session) */
 
 import { createServer } from "http"
-import { Server } from "socket.io"
+/* import { Server } from "socket.io"
 import Socket from "./src/socket/Socket.js";
 
-import MongoStore from "connect-mongo"
+import MongoStore from "connect-mongo" */
 
 /* const options = {default: { PORT: 8080 }, alias: { p: "PORT"}}
 const args = parseArgs(process.argv.slice(2), options) */
 const app = express();
 /* const PORT = args.PORT */
 const httpServer = new createServer(app)
-const io = new Server(httpServer)
-const socket = new Socket(io)
+/* const io = new Server(httpServer)
+const socket = new Socket(io) */
 
 const PORT = process.env.PORT || 8080
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+/* app.use(express.json())
+app.use(express.urlencoded({ extended: true })) */
 app.use(express.static('./public'));
 
 
 
 //Posicionarlo arriba de las rutas ya que se lo asigna por orden
-app.use(session({
+/* app.use(session({
     store: MongoStore.create({mongoUrl: config.mongodb.cnxStr}),
     secret: 'secreto',
     resave: true,
     rolling: true,
     saveUninitialized: true,
     //cookie: { maxAge: 60000 } // 60 segundos
-}))
-
+})) */
+/* 
 app.use('/api/products', productRouter);
 app.use('/api/messages', messageRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/randoms', randomRouter);
-app.use('/api/',generalRouter);
+app.use('/api/',generalRouter); */
 app.use('/products', productWebRouter);
 app.use('/messages', messageWebRouter);
 app.use('/cart', cartWebRouter);
@@ -65,7 +65,7 @@ app.use('/info',infoWebRouter);
 app.use('/',generalWebRouter);
 
 const server = httpServer.listen(PORT, async () => {
-    console.log(`Servidor Corriendo en el puerto: ${server.address().port}`)
+    //console.log(`Servidor Corriendo en el puerto: ${server.address().port}`)
     console.log(`Servidor Corriendo en el puerto: ${PORT}`)
 });
 
@@ -80,10 +80,10 @@ app.engine('handlebars', handlebars.engine())
 app.set('views', './public')
 app.set('view engine', 'handlebars')
 
-io.on('connection', socket.connection)
+/* io.on('connection', socket.connection) */
 
-app.use((req, res) => {
+/* app.use((req, res) => {
     res.status(404);
     res.json(new ServiceException(-2, `Ruta ${req.originalUrl} método ${req.method} no implementada.`))
-})   
+})    */
 
