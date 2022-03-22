e2b110b1387e10687c41b8671d52b0ac98d5244b
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-03-22 00:43:02 -0300

    heroku14

--- index.js
+++ index.js
@@ -1,90 +1,24 @@
-/* import ServiceException from "./src/exceptions/ServiceException.js"
-import config from './options/config.js'
-import parseArgs from "minimist";
-import { fork } from 'child_process'
+const express = require('express')
+const app = express()
 
-import { generalRouter } from "./src/routers/api/GeneralRouter.js";
-import { productRouter } from "./src/routers/api/ProductRouter.js";
-import { messageRouter } from "./src/routers/api/MessageRouter.js";
-import { cartRouter } from "./src/routers/api/CartRouter.js";
-import { randomRouter } from "./src/routers/api/RandomRouter.js";
-
-import { productWebRouter } from "./src/routers/web/ProductWebRouter.js";
-import { messageWebRouter } from "./src/routers/web/MessageWebRouter.js";
-import { generalWebRouter } from "./src/routers/web/GeneralWebRouter.js";
-import { cartWebRouter } from "./src/routers/web/cartWebRouter.js";
-import { infoWebRouter } from "./src/routers/web/InfoWebRouter.js"; */
-
-import express from 'express'
-import handlebars from 'express-handlebars'
-/* import session from 'express-session'
-import sessionFile from 'session-file-store'
-sessionFile(session) */
-
-import { createServer } from "http"
-/* import { Server } from "socket.io"
-import Socket from "./src/socket/Socket.js";
-
-import MongoStore from "connect-mongo" */
-
-//const options = {default: { PORT: 8080 }, alias: { p: "PORT"}}
-//const args = parseArgs(process.argv.slice(2), options)
-const app = express();
-//const PORT = args.PORT
-const PORT = 8080
-const httpServer = new createServer(app)
-/* const io = new Server(httpServer)
-const socket = new Socket(io) */
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('./public'));
-
-//Posicionarlo arriba de las rutas ya que se lo asigna por orden
-/* app.use(session({
-    store: MongoStore.create({mongoUrl: config.mongodb.cnxStr}),
-    secret: 'secreto',
-    resave: true,
-    rolling: true,
-    saveUninitialized: true,
-    //cookie: { maxAge: 60000 } // 60 segundos
-})) */
-
-/* app.use('/api/products', productRouter);
-app.use('/api/messages', messageRouter);
-app.use('/api/carts', cartRouter);
-app.use('/api/randoms', randomRouter);
-app.use('/api/',generalRouter);
-app.use('/products', productWebRouter);
-app.use('/messages', messageWebRouter);
-app.use('/cart', cartWebRouter);
-app.use('/info',infoWebRouter);
-app.use('/',generalWebRouter); */
-
-const server = httpServer.listen(PORT, async () => {
-    console.log(`Servidor Corriendo en el puerto: ${server.address().port}`)
-});
-
-app.get('/', (req,res) => {
-    console.log('Home');
-    res.json("hola");
-})
-
-/* server.on('error', function (e) {
-    console.log('Error al conectar con el servidor');
-    console.log(e);
-}); */
-
-//handlebars
+app.use(express.static('public'))
 
 app.engine('handlebars', handlebars.engine())
 app.set('views', './public')
 app.set('view engine', 'handlebars')
 
-//io.on('connection', socket.connection)
+app.get('/', (req,res) => {
+    console.log('Home');
+    res.send("index");
+})
 
-/* app.use((req, res) => {
-    res.status(404);
-    res.json(new ServiceException(-2, `Ruta ${req.originalUrl} método ${req.method} no implementada.`))
-})    */
+app.get('/mensaje', (req,res) => {
+    console.log('Mensajes');
+    res.send("mensaje");
+})
 
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
+})
+server.on('error', error => console.log(`Error en el servidor ${error}`))
