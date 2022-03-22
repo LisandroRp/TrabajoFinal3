0a97d61d1715df102382375d41b0ebd843da1c75
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-03-22 00:24:02 -0300

    heroku9

--- options/config.js
+++ options/config.js
@@ -3,6 +3,9 @@ import 'dotenv/config'
 export default {
 /*     mongodb: JSON.parse(process.env.MONGODB),
     firebase: JSON.parse(process.env.FIREBASE) */
-    mongodb:  JSON.parse({"cnxStr":"mongodb+srv://admin:admin@cluster0.msysu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority","options":{"useNewUrlParser": true,"useUnifiedTopology":true,"serverSelectionTimeoutMS":5000}}),
+    mongodb:  {
+        cnxStr:"mongodb+srv://admin:admin@cluster0.msysu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        options:{"useNewUrlParser": true,"useUnifiedTopology":true,"serverSelectionTimeoutMS":5000}
+    },
     //firebase: JSON.parse(process.env.FIREBASE)
 }
