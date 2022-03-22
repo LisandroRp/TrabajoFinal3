3cf3b479e3213776d3bd3e252771609a2a7519a9
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-03-22 00:10:26 -0300

    heroku6

--- options/config.js
+++ options/config.js
@@ -2,5 +2,5 @@ import 'dotenv/config'
 
 export default {
     mongodb: process.env.MONGODB ? JSON.parse(process.env.MONGODB) : "mongodb+srv://admin:admin@cluster0.msysu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    firebase: JSON.parse(process.env.FIREBASE)
+    //firebase: JSON.parse(process.env.FIREBASE)
 }
