0ee5f78d840fd248f0501ea80fa2fca8a1b770ec
Lisandro Rodriguez Prados <lisandrorp1997@gmail.com>
2022-03-25 17:06:01 -0300

    Heroku sin routes16

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ const socket = new Socket(io) */
 
 const PORT = process.env.PORT || 8080
 
-/* app.use(express.json())
-app.use(express.urlencoded({ extended: true })) */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('./public'));
 
 
